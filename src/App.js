f5ba40573f6b1cd1027f4a06a2b93c01321e0c2b
AmanDsan <dubeyaman063@gmail.com>
2023-05-18 11:50:44 +0530

    quote generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import quotes from "./quotes.js";
 
-function App() {
+export default function App() {
+  const [index, setIndex] = useState(0);
+  const handleChange = () => {
+    setIndex(Math.floor(Math.random() * 10));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainBox">
+      <span>{quotes[index].quote}</span>
+      <p>-{quotes[index].by}</p>
+      <button onClick={handleChange}>Change Quote</button>
     </div>
   );
 }
-
-export default App;
